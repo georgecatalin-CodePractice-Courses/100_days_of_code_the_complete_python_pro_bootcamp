f34f8b0a15057657e3cf90015b5b886f3f13ce41
George Calin <george.calin@gmail.com>
2022-04-12 13:54:56 +0000

    day 1 assignments completed

---
+++ Day 1/assignment_3.py
@@ -0,0 +1,5 @@
+#Write your code below this line 👇
+
+user=input("")
+print("What is your name?{0}".format(len(user)))
+
---
+++ Day 1/assignment_4.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temporary = b
+b = a
+a = temporary
+
+
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
